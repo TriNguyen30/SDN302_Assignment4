85c330a76bc6a793a3189b0dd96f1f2ddd6ffa95
HenryNguyen30 <trinc0201@gmail.com>
2025-12-12 15:30:00 +0700

    fix cors

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://quizzesv1.vercel.app/'],
   credentials: false,
 }));
 
